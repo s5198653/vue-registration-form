2ce7d42d95f4a6ea510df935f74b4e50af4efa09
Taranovskaya <s5198653@yandex.ru>
2025-03-22 14:30:50 +0300

    create VInput component

---
+++ src/components/VInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { type = 'text' } = defineProps<{
+  type: 'text' | 'password' | 'email' | 'number' | 'checkbox' | 'radio' | 'tel';
+}>();
+const model = defineModel();
+</script>
+
+<template>
+  <input v-model="model" :type />
+</template>
+
+<style scoped></style>
