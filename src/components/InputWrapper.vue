70289217100849f76f01559dd998a5cbdcc3b3cf
Taranovskaya <s5198653@yandex.ru>
2025-03-22 13:54:57 +0300

    create InputWrapper component

---
+++ src/components/InputWrapper.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ label: string; error: string }>();
+</script>
+
+<template>
+  <label class="input-wrapper">
+    <span>{{ label }}</span>
+    <slot />
+    <span>{{ error }}</span>
+  </label>
+</template>
+
+<style scoped>
+.input-wrapper {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+</style>
