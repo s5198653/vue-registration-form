53491eb431167ad1019a0e8bb2b05769da1e880a
Taranovskaya <s5198653@yandex.ru>
2025-03-22 13:33:47 +0300

    create RegistrationForm component

---
+++ src/components/RegistrationForm.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+type FormFields = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: 'male' | 'female' | 'reptilian' | 'prefer not to say';
+  isConfirmTerms: boolean;
+};
+
+const formFields = reactive<FormFields>({
+  firstName: '',
+  lastName: '',
+  phone: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  gender: 'prefer not to say',
+  isConfirmTerms: false,
+});
+</script>
+
+<template>
+  <form></form>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RegistrationForm from '@/components/RegistrationForm.vue';
+</script>
 
 <template>
-  <main>hello</main>
+  <main>
+    <RegistrationForm />
+  </main>
 </template>
