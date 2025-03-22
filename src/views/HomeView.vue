cac4d3a2f8b7a8240f00ec953ed869a25445a7ff
Taranovskaya <s5198653@yandex.ru>
2025-03-22 17:03:12 +0300

    add title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import RegistrationForm from '@/components/RegistrationForm.vue';
+import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+
+const route = useRoute();
+
+onMounted(() => {
+  document.title = route.meta.title as string;
+});
 </script>
 
 <template>
