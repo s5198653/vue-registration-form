63f58ff0afbb37dcab64104419c77fa1d837d3ff
Taranovskaya <s5198653@yandex.ru>
2025-03-24 18:00:21 +0300

    creates greeting page

---
+++ src/views/GreetingView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+import VTitle from '@/components/VTitle.vue';
+
+const route = useRoute();
+
+onMounted(() => {
+  document.title = route.meta.title as string;
+});
+</script>
+
+<template>
+  <main>
+    <VTitle
+      tag="h1"
+      class="registration-form__title"
+      :style="{ color: 'white', fontWeight: 'bold' }"
+      >Hello!</VTitle
+    >
+  </main>
+</template>
