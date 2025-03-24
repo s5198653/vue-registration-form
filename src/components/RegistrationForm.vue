15afbb9423b5f811479ed480bcf714ffae695a2b
Taranovskaya <s5198653@yandex.ru>
2025-03-24 17:59:23 +0300

    adds form submission

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { computed, reactive, watch } from 'vue';
+import { computed, reactive, watch, ref } from 'vue';
 import VTitle from './VTitle.vue';
 import InputWrapper from './InputWrapper.vue';
 import VInput from './VInput.vue';
 import VButton from './VButton.vue';
 import VRadioGroup from './VRadioGroup.vue';
+import { sendRegistrationForm } from '@/api/fakeBackend';
+import router from '@/router';
 
 enum Gender {
   Male = 'male',
@@ -13,7 +15,7 @@ enum Gender {
   PreferNotToSay = 'prefer not to say',
 }
 
-type Validator = (fieldName: keyof FormFields, confirmFieldName?: keyof FormFields) => boolean;
+type Validator = (fieldName: keyof FormFields, fieldForMatching?: keyof FormFields) => boolean;
 
 type FormField<T extends string | boolean | Gender = string> = {
   value: T;
@@ -37,6 +39,10 @@ type PreparedFormFields = {
   [K in keyof FormFields]: FormFields[K]['value'];
 };
 
+type BackendError = {
+  [k in keyof FormFields | 'nonFieldError']?: string;
+};
+
 const formFields = reactive<FormFields>({
   firstName: { value: '', error: '', validators: [isRequired] },
   lastName: { value: '', error: '', validators: [isRequired] },
@@ -53,6 +59,10 @@ const isFormValid = computed(() => {
   return Object.values(formFields).every((field) => !field.error);
 });
 
+const isLoading = ref(false);
+const backendNonFieldError = ref('');
+const showErrorAnimation = ref(false);
+
 const preparedFormFields = computed<PreparedFormFields>(() => {
   return Object.fromEntries(
     Object.entries(formFields).map(([key, value]) => [key, value.value]),
@@ -65,49 +75,111 @@ function isRequired(fieldName: keyof FormFields): boolean {
   return isValid;
 }
 function isValidPhoneNumber(fieldName: keyof FormFields): boolean {
+  const fieldValue = formFields[fieldName].value;
+  if (!fieldValue) return true;
   const regex = /^\+?[1-9]\d{1,14}$/;
-  const isValid = regex.test(String(formFields[fieldName].value));
-  formFields[fieldName].error = isValid ? '' : 'Invalid phone number';
+  const isValid = regex.test(String(fieldValue));
+  formFields[fieldName].error = isValid ? '' : 'Invalid phone number format. Use +?[1-9]\d{1,14}.';
   return isValid;
 }
 
 function isValidEmail(fieldName: keyof FormFields): boolean {
+  const fieldValue = formFields[fieldName].value;
+  if (!fieldValue) return true;
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const isValid = regex.test(String(formFields[fieldName].value));
-  formFields[fieldName].error = isValid ? '' : 'Invalid email';
+  const isValid = regex.test(String(fieldValue));
+  formFields[fieldName].error = isValid
+    ? ''
+    : 'Invalid email format. Must match pattern: username@domain.com."';
   return isValid;
 }
 
 function isValidPassword(fieldName: keyof FormFields): boolean {
+  const fieldValue = formFields[fieldName].value;
+  if (!fieldValue) return true;
   const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]).{8,}$/;
-  const isValid = regex.test(String(formFields[fieldName].value));
-  formFields[fieldName].error = isValid ? '' : 'Invalid password';
+  const isValid = regex.test(String(fieldValue));
+  formFields[fieldName].error = isValid
+    ? ''
+    : 'Password: 8+ chars, uppercase, lowercase, number, special.';
   return isValid;
 }
 
 function isPasswordConfirm(
   fieldName: keyof FormFields,
-  confirmFieldName?: keyof FormFields,
+  fieldForMatching?: keyof FormFields,
 ): boolean {
-  if (!confirmFieldName) return false;
-  const isValid = formFields[fieldName].value === formFields[confirmFieldName].value;
+  if (!fieldForMatching) return false;
+  const isValid = formFields[fieldName].value === formFields[fieldForMatching].value;
   formFields[fieldName].error = isValid ? '' : 'Passwords do not match.';
   return isValid;
 }
 
 function validate(fieldName: keyof FormFields) {
+  formFields[fieldName].error = '';
   const validators = formFields[fieldName].validators;
   validators.forEach((validator) => {
-    console.log('validator :>> ', validator);
     validator(fieldName, fieldName === 'confirmPassword' ? 'password' : undefined);
   });
 }
 
-function onSubmit() {
+function validateOnSubmit() {
   Object.entries(formFields).forEach(([fieldName]) => {
     validate(fieldName as keyof FormFields);
   });
-  console.log('preparedFormFields :>> ', preparedFormFields.value);
+}
+
+function resetForm() {
+  Object.entries(formFields).forEach(([fieldName]) => {
+    if (fieldName === 'isConfirmTerms' || fieldName === 'receiveNews') {
+      formFields[fieldName as keyof FormFields].value = false;
+    } else if (fieldName === 'gender') {
+      formFields[fieldName as keyof FormFields].value = Gender.PreferNotToSay;
+    } else {
+      formFields[fieldName as keyof FormFields].value = '';
+    }
+  });
+}
+
+function handleBackendErrors(error: BackendError) {
+  showErrorAnimation.value = true;
+  if (error.nonFieldError) {
+    backendNonFieldError.value = error.nonFieldError;
+    return;
+  }
+  const errorFieldNames = Object.keys(error) as (keyof FormFields)[];
+  Object.keys(formFields).forEach((fieldName) => {
+    if (errorFieldNames.includes(fieldName as keyof FormFields)) {
+      const currentErrorFieldName = errorFieldNames.find(
+        (name: keyof FormFields) => name === fieldName,
+      );
+      formFields[fieldName as keyof FormFields].error = currentErrorFieldName
+        ? (error[currentErrorFieldName] as string)
+        : '';
+    }
+  });
+}
+
+function resetBackendErrors() {
+  backendNonFieldError.value = '';
+  showErrorAnimation.value = false;
+}
+
+async function onSubmit() {
+  resetBackendErrors();
+  validateOnSubmit();
+  if (!isFormValid.value) return;
+
+  try {
+    isLoading.value = true;
+    await sendRegistrationForm(preparedFormFields.value);
+    resetForm();
+    router.push({ name: 'greeting' });
+  } catch (error) {
+    handleBackendErrors(error as BackendError);
+  } finally {
+    isLoading.value = false;
+  }
 }
 
 watch(
@@ -117,9 +189,17 @@ watch(
 </script>
 
 <template>
-  <form class="registration-form" @submit.prevent="onSubmit">
+  <form
+    :class="[
+      'registration-form',
+      {
+        ['registration-form--error']: showErrorAnimation,
+      },
+    ]"
+    @submit.prevent="onSubmit"
+  >
     <VTitle tag="h2" class="registration-form__title">Please register</VTitle>
-    <div class="registration-form__fields">
+    <div class="registration-form__text-fields">
       <InputWrapper label="First name" :error="formFields.firstName.error">
         <VInput
           v-model="formFields.firstName.value"
@@ -127,7 +207,7 @@ watch(
           @input="validate('firstName')"
         />
       </InputWrapper>
-      <InputWrapper label="Last Name" :error="formFields.lastName.error">
+      <InputWrapper label="Last name" :error="formFields.lastName.error">
         <VInput
           v-model="formFields.lastName.value"
           placeholder="Enter your last name"
@@ -167,55 +247,101 @@ watch(
         />
       </InputWrapper>
     </div>
-    <InputWrapper label="Choice gender" :error="formFields.gender.error">
-      <VRadioGroup
-        name="gender"
-        :options="Object.values(Gender)"
-        v-model="formFields.gender.value"
-      />
-    </InputWrapper>
-    <InputWrapper label="Receive news" :error="formFields.receiveNews.error" horizontal>
-      <VInput
-        v-model="formFields.receiveNews.value"
-        type="checkbox"
-        @input="validate('receiveNews')"
-      />
-    </InputWrapper>
-    <InputWrapper label="Confirm terms" :error="formFields.isConfirmTerms.error" horizontal>
-      <VInput
-        v-model="formFields.isConfirmTerms.value"
-        type="checkbox"
-        @input="validate('isConfirmTerms')"
-      />
-    </InputWrapper>
-    <VButton type="submit" :disabled="!isFormValid">Register</VButton>
+    <div class="registration-form__check-fields">
+      <InputWrapper label="Choice gender" :error="formFields.gender.error">
+        <VRadioGroup
+          name="gender"
+          :options="Object.values(Gender)"
+          v-model="formFields.gender.value"
+        />
+      </InputWrapper>
+      <InputWrapper label="Receive news" :error="formFields.receiveNews.error" horizontal>
+        <VInput
+          v-model="formFields.receiveNews.value"
+          type="checkbox"
+          @change="validate('receiveNews')"
+        />
+      </InputWrapper>
+      <InputWrapper label="Confirm terms" :error="formFields.isConfirmTerms.error" horizontal>
+        <VInput
+          v-model="formFields.isConfirmTerms.value"
+          type="checkbox"
+          @change="validate('isConfirmTerms')"
+        />
+      </InputWrapper>
+    </div>
+    <VButton type="submit" :disabled="!isFormValid || isLoading" :loading="isLoading"
+      >Register</VButton
+    >
+    <span v-if="backendNonFieldError" class="registration-form__error"
+      >{{ backendNonFieldError }} Try register again.</span
+    >
   </form>
 </template>
-
+`
 <style scoped>
 .registration-form {
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
   max-width: 700px;
   width: 100%;
+
+  position: relative;
   background-color: #fff;
-  padding: 25px 30px;
+  padding: 30px 25px;
   border-radius: 5px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+}
+
+.registration-form--error {
+  border: 1px solid red;
+  box-shadow: inset 0 0 5px red;
+  animation: shake 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) both;
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translateX(-0.5px);
+  }
+  20%,
+  80% {
+    transform: translateX(1px);
+  }
+  30%,
+  50%,
+  70% {
+    transform: translateX(-2px);
+  }
+  40%,
+  60% {
+    transform: translateX(2px);
+  }
 }
 
 .registration-form__title {
   font-size: 28px;
 }
 
-.registration-form__fields {
+.registration-form__text-fields {
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 10px 30px;
+  column-gap: 30px;
 
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
+}
+
+.registration-form__check-fields {
+  display: flex;
+  flex-direction: column;
+}
+.registration-form__error {
+  position: absolute;
+  bottom: 10px;
+  font-size: 12px;
+  color: red;
 }
 </style>
